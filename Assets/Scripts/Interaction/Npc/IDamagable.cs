d6e63a536e9f3f208fe7cff94b4480c87af954f2
Nikolai Selivanov <nikolaiselivanov@Nikolais-MacBook-Pro.local>
2024-01-27 13:02:54 +0200

    Fix binding

---
+++ Assets/Scripts/Interaction/Npc/IDamagable.cs
@@ -0,0 +1,7 @@
+namespace LaughGame.Interaction.Npc
+{
+    public interface IDamagable
+    {
+        void TakeDamage(float damage);
+    }
+}
--- Assets/Scripts/Interaction/Npc/IDamageReceiver.cs
+++ Assets/Scripts/Interaction/Npc/IDamageReceiver.cs
@@ -2,6 +2,7 @@ namespace LaughGame.Interaction.Npc
 {
     public interface IDamageReceiver
     {
+        void SetDamagable(IDamagable damagable);
         void DoDamage(float damage);
     }
 }
