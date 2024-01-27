c38d9cb34cd325a69b42c8e9346d678b01ce6185
Nikolai <kekchpekk@gmail.com>
2024-01-27 11:55:13 +0200

    Enemy layer

---
+++ Assets/Scripts/Model/Abilities/AbilitiesConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaughGame.Assets.Scripts.Model.Abilities
+{
+    static class AbilitiesConfig
+    {
+        public static int EnemiesLayerMask = 10;
+    }
+}
---
+++ Assets/Scripts/Model/Abilities/CircleAbility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LaughGame.Assets.Scripts.Model.Abilities
+{
+    class CircleAbility : MonoBehaviour, IAbility
+    {
+
+        [field: SerializeField] public Transform AbilityParent { get; private set; }
+
+        public void Execute()
+        {
+
+            var bodies = Physics.OverlapSphere(AbilityParent.position, 10f, AbilitiesConfig.EnemiesLayerMask);
+
+
+
+        }
+
+      
+    }
+}
